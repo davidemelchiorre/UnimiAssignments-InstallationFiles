0539c7bf8dfd0e259f66604d4fb7ea5e2d5b8132
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-12 12:01:26 +0100

    Update CliInterface.py

--- CliInterface.py
+++ CliInterface.py
@@ -2,7 +2,7 @@ import subprocess
 print ""
 print "Starting Cli Interface..."
 print "Press CTRL+C to terminate"
-print ""
+print "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"
 print "-------------------------Cli Interface-------------------------"
 print ""
 while 1:
