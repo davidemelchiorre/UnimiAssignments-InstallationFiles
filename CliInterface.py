23afbbfa0644a4d63ff4796d9e7397f87e96897e
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-12 18:48:23 +0100

    Update CliInterface.py

--- CliInterface.py
+++ CliInterface.py
@@ -56,8 +56,6 @@ def get():
 
 def quit():
     server.close()
-    subprocess.call('lsof -n -i', shell=True)
-    subprocess.call('exit', shell=True)
     os.kill(os.getpid(),15)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
