798b4da04f666a996a4b4fbee41e7dbf206a5c32
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-12 17:58:54 +0100

    Update CliInterface.py

--- CliInterface.py
+++ CliInterface.py
@@ -7,9 +7,52 @@ host=''
 port = 9696
 buflen=1024
 
-print ""
-print "Starting Cli Interface..."
-print "Press CTRL+C to terminate"
+def start():
+    print ""
+    print "-----------------Start routine-----------------"
+    try:
+        if proc is None:
+            print("Info>> Starting program...")
+            proc=subprocess.Popen(["jupyter","notebook"])
+        else:
+            print("Info>> Program is already running")
+    except NameError:
+        print("Info>> Starting program...")
+        proc=subprocess.Popen(["jupyter","notebook"])
+    print "-----------------------------------------------"
+    print ""
+    
+def stop():
+    print ""
+    print "-----------------Stop routine------------------"
+    try:
+        if proc is not None:
+            print(proc)
+            print("Info>> Stopping program...")
+            proc.kill()
+            proc=None
+        else:
+            print("Info>> Program is not running")
+    except NameError:
+        print("Info>> Program is not running")
+    print "-----------------------------------------------"
+    print ""
+
+def get():
+    print ""
+    print "--------------Getting Notebooks----------------"
+    print "Cloning into Esercitazioni..."
+    subprocess.call('rm -r Esercitazioni', shell=True)
+    subprocess.call('git clone https://github.com/davidemelchiorre/UnimiAssignment-Esercitazioni.git Esercitazioni', shell=True)
+    subprocess.call('ls -l', shell=True)
+    print "cloned into Esercitazioni"
+    print "-----------------------------------------------"
+    print ""
+
+def quit():
+    server.close()
+    subprocess.call('lsof -n -i', shell=True)
+    subprocess.call('exit', shell=True)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
@@ -23,71 +66,39 @@ def server_function():
         buf = client.recv(buflen)
         if not buf:break
         comando=buf.decode('utf-8')
-        print ">",comando
+        
+        if comando!="Hello World"
+            print ">",comando
+        if comando=="start":
+            start()
+        if comando=="stop":
+            stop()
+        if comando=="get":
+            get()
         if comando=="quit":
             client.close()
             client.send(comando.encode('utf-8'))
-        
-
+            quit()
 
 
 server_thread=threading.Thread(name='server_thread', target=server_function)
 server_thread.start()
 
+print ""
+print "Starting Cli Interface..."
+print "Type 'quit' to terminate"
 #------------------------------------------------------------------------------------
 print ""
 print "-------------------------Cli Interface-------------------------"
 print ""
 while 1:
     
-    if comando=="Hello World":
-        comando = raw_input(">")
-    
+    comando = raw_input(">")
     if comando=="start":
-        print ""
-        print "-----------------Start routine-----------------"
-        try:
-            if proc is None:
-                print("Info>> Starting program...")
-                proc=subprocess.Popen(["jupyter","notebook"])
-            else:
-                print("Info>> Program is already running")
-        except NameError:
-            print("Info>> Starting program...")
-            proc=subprocess.Popen(["jupyter","notebook"])
-        print "-----------------------------------------------"
-        print ""
-  
+        start()
     if comando=="stop":
-        print ""
-        print "-----------------Stop routine------------------"
-        try:
-            if proc is not None:
-                print(proc)
-                print("Info>> Stopping program...")
-                proc.kill()
-                proc=None
-            else:
-                print("Info>> Program is not running")
-        except NameError:
-            print("Info>> Program is not running")
-        print "-----------------------------------------------"
-        print ""
-        
+        stop()
     if comando=="get":
-        print ""
-        print "--------------Getting Notebooks----------------"
-        print "Cloning into Esercitazioni..."
-        subprocess.call('rm -r Esercitazioni', shell=True)
-        subprocess.call('git clone https://github.com/davidemelchiorre/UnimiAssignment-Esercitazioni.git Esercitazioni', shell=True)
-        subprocess.call('ls -l', shell=True)
-        print "cloned into Esercitazioni"
-        print "-----------------------------------------------"
-        print ""
-    
+        get()
     if comando=="quit":
-        server.close()
-        subprocess.call('lsof -n -i', shell=True)
-        subprocess.call('exit', shell=True)
-
-    comando="Hello World"
+        quit()
