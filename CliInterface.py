7f99c73a67f006a815fe3eca61cba4a772b8c9e4
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-12 12:00:55 +0100

    Update CliInterface.py

--- CliInterface.py
+++ CliInterface.py
@@ -4,4 +4,39 @@ print "Starting Cli Interface..."
 print "Press CTRL+C to terminate"
 print ""
 print "-------------------------Cli Interface-------------------------"
-
+print ""
+while 1:
+    comando = raw_input(">")
+    
+    if comando=="start":
+        print ""
+        print "-----------------Start routine-----------------"
+        try:
+            if proc is None:
+                print("Info>> Starting program...")
+                subprocess.call(["ls","-l"])
+                proc=subprocess.Popen(["jupyter","notebook"])
+            else:
+                print("Info>> Program is already running")
+        except NameError:
+            print("Info>> Starting program...")
+            subprocess.call(["ls","-l"])
+            proc=subprocess.Popen(["jupyter","notebook"])
+        print "-----------------------------------------------"
+        print ""
+  
+    if comando=="stop":
+        print ""
+        print "-----------------Stop routine------------------"
+        try:
+            if proc is not None:
+                print(proc)
+                print("Info>> Stopping program...")
+                proc.kill()
+                proc=None
+            else:
+                print("Info>> Program is not running")
+        except NameError:
+            print("Info>> Program is not running")
+        print "-----------------------------------------------"
+        print ""
