c8a878af015679f40c8439b21e2caec8aa65e228
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-12 13:53:59 +0100

    Update CliInterface.py

--- CliInterface.py
+++ CliInterface.py
@@ -44,6 +44,7 @@ while 1:
         print ""
         print "-----------------Pulling Repository------------------"
         subprocess.call('cd Esercitazioni', shell=True)
+        subprocess.call('ls -l', shell=True)
         subprocess.call('git pull https://github.com/davidemelchiorre/UnimiAssignment-Esercitazioni.git', shell=True)
         print "-----------------------------------------------"
         print ""
