a051331333b0677be4e0e90753ae259002ab0f06
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-12 14:43:55 +0100

    Update pull_Esercitazioni.py

--- Esercitazioni/pull_Esercitazioni.py
+++ Esercitazioni/pull_Esercitazioni.py
@@ -2,3 +2,5 @@ import subprocess
 
 subprocess.call('ls -l', shell=True)
 subprocess.call('git pull https://github.com/davidemelchiorre/UnimiAssignment-Esercitazioni.git', shell=True)
+subprocess.call('ls -l', shell=True)
+print "pulled into Esercitazioni"
