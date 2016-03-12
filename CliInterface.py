a5d19abda229fb53ef3cfe22241b8a3f03762657
davidemelchiorre <davide.melchiorre@studenti.unimi.it>
2016-03-12 13:32:01 +0100

    Update CliInterface.py

--- CliInterface.py
+++ CliInterface.py
@@ -15,13 +15,13 @@ while 1:
         try:
             if proc is None:
                 print("Info>> Starting program...")
-                subprocess.call("cd","../notebook")
+                subprocess.Popen(["cd",".."])
                 proc=subprocess.Popen(["jupyter","notebook"])
             else:
                 print("Info>> Program is already running")
         except NameError:
             print("Info>> Starting program...")
-            subprocess.call("cd","../notebook")
+            subprocess.Popen(["cd",".."])
             proc=subprocess.Popen(["jupyter","notebook"])
         print "-----------------------------------------------"
         print ""
